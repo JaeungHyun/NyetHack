95588f4c9f8fef3fca023cc407f680b4bbd09d8a
JaeUng Hyun <jaeung@me.com>
2019-05-28 17:37:39 +0900

    add menu file and update until chapter 10

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,16 +1,48 @@
 import kotlin.math.roundToInt
+import java.io.File
+
 const val TAVERN_NAME = "Taernyl's Folly"
 
 var playerGold = 10
 var playerSilver = 10
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "fernsworth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
+val menuList = File("data/tavern-menu-items.txt").readText().split("\n") // 윈도우에서는 "\r\n" 으로 구분자를 넣어줘야함
 
 fun main() {
-    placeOrder("Shandy,DrAgOn's BrEAth,5.91")
+    if (patronList.contains("Eli")) {
+        println("술집 주인이 말한다: Eli는 안쪽 방에서 카드하고 있어요.")
+    } else {
+        println("술집 주인이 말한다: Eli는 여기 없어요.")
+    }
+
+    if (patronList.containsAll(listOf("Mordoc", "Sophie"))) {
+        println("술집 주인이 말한다: 네, 모두 있어요.")
+    } else {
+        println("술집 주인이 말한다: 아니오, 나간 사람도 있습니다")
+    }
+
+    (0..9).forEach {
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first $last"
+        uniquePatrons += name
+    }
+    println(uniquePatrons)
+
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.shuffled().first(),
+            menuList.shuffled().first())
+        orderCount++
+    }
 }
 
 
-fun performPurchase(price: Double) {
+fun performPurchase(price: Double, name: String, patronName: String) {
     displayBalance()
+
     val totalPurse = playerGold + (playerSilver / 100.0)
     println("지갑 전체 금액: 금화 $totalPurse")
 
@@ -24,11 +56,18 @@ fun performPurchase(price: Double) {
         val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
         playerGold = remainingGold
         playerSilver = remainingSilver
+
         displayBalance()
+
+        val phrase = if (name == "DrAgOn's BrEAth") {
+            "$patronName 이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}"
+        } else {
+            "$patronName 이 말한다: 감사합니다 $name."
+        }
+
+        println(phrase)
     } else {
-        val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
-        val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-        print("$tavernMaster : 잔액이 부족해 구입할 수 없습니다.")
+        println("술집 주인이 말한다: 잔액이 부족해 구입할 수 없습니다.")
     }
 }
 
@@ -51,24 +90,17 @@ private fun toDragonSpeak(pharse: String) =
     }
 
 
-private fun placeOrder(menuData: String) {
+private fun placeOrder(patronName: String, menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("마드리갈은 $tavernMaster 에게 주문한다.")
+    println("$patronName 은 $tavernMaster 에게 주문한다.")
 
     val (type, name, price) = menuData.split(',')
-    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+    val message = "$patronName 은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
 
-    performPurchase(price.toDouble())
+    performPurchase(price.toDouble(), name, patronName)
 
-    val phrase = if (name == "DrAgOn's BrEAth") {
-        println("마드리갈이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}")
-    } else {
-        "마드리갈이 말한다: 감사합니다 $name."
-    }
-
-    println(phrase)
 }
 
 
